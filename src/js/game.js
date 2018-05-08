f0e25f8d65dc04e04b35b7dea6c2e8c3bd2ba707
Safira Nugroho <snugroho@thoughtworks.com>
2018-05-08 17:22:22 +1000

    Define hands and what they win over

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,22 @@
+const hands = {
+  'rock': {
+    winsOver: 'scissors'
+  },
+  'scissors': {
+    winsOver: 'paper'
+  },
+  'paper': {
+    winsOver: 'rock'
+  }
+}
+
 const play = (firstHand, secondHand) => {
-  return 0;
+  if (firstHand === secondHand) return 0;
+  if (hands[firstHand].winsOver === secondHand) {
+    return 1;
+  } else if (hands[secondHand].winsOver === firstHand) {
+    return 2;
+  }
 }
 
 module.exports = {
--- test/game.test.js
+++ test/game.test.js
@@ -5,4 +5,16 @@ describe('game', () => {
     let winner = play('rock', 'rock');
     expect(winner).toBe(0);
   });
+  it('should return 1 if rock is first hand and it wins', () => {
+    let winner = play('rock', 'scissors');
+    expect(winner).toBe(1);
+  });
+  it('should return 2 if rock is second hand and it wins', () => {
+    let winner = play('scissors', 'rock');
+    expect(winner).toBe(2);
+  });
+  it('should return 1 if paper is first hand and it wins', () => {
+    let winner = play('paper', 'rock');
+    expect(winner).toBe(1);
+  })
 });
