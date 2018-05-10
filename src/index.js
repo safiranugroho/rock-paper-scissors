0cc01a6e11877e47771dececc3cdadbcf6ad63ec
Safira Nugroho <snugroho@thoughtworks.com>
2018-05-10 16:09:02 +1000

    Both hands are playing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ const { game } = require('./js/game');
 
 const playGame = () => {
   const firstPlayer = game.shoot();
+  document.getElementById('first-player-hand')
+          .innerHTML = firstPlayer;
+
   const secondPlayer = game.shoot();
+  document.getElementById('second-player-hand')
+          .innerHTML = secondPlayer;
 
   game.play(firstPlayer, secondPlayer);
 }
