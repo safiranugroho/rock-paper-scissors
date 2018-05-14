cc0edbc1180ba42003186fc5d123d1f01796f75c
Safira Nugroho <snugroho@thoughtworks.com>
2018-05-14 11:27:59 +1000

    Translate index.test.js to a cypress test

---
+++ cypress/integration/index_spec.js
@@ -0,0 +1,37 @@
+const homepage = 'http://localhost:8080'
+
+describe('Rock Paper Scissors', () => {
+  context('Random play', () => {
+    it('should display nothing as first player\'s default hand', () => {
+      cy.visit(homepage);
+      cy.get('#first-player-hand')
+        .should('be.empty');
+    });
+
+    it('should display nothing as second player\'s default hand', () => {
+      cy.visit(homepage);
+      cy.get('#second-player-hand')
+        .should('be.empty');
+    });
+
+    it('should update player hands and winner after play button is clicked', () => {
+      cy.visit(homepage);
+
+      cy.get('#winner')
+        .should('be.empty');
+
+      cy.get('#play-button')
+        .click();
+
+      cy.get('#first-player-hand')
+        .should('not.be.empty');
+
+      cy.get('#second-player-hand')
+        .should('not.be.empty');
+
+      cy.get('#winner')
+        .should('not.be.empty');
+    });
+  });
+
+})
