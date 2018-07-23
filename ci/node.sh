80142c9616e21484378f4d99134fd69dbbcf5c49
Safira Nugroho <snugroho@thoughtworks.com>
2018-07-23 23:35:09 +1000

    :)

--- ci/node.sh
+++ ci/node.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 ROOT_FOLDER=$( pwd )
-export NPM_CONFIG_PREFIX=$ROOT_FOLDER/node_modules
+ln -s $ROOT_FOLDER/node_modules $ROOT_FOLDER/rock-paper-scissors/rps/node_modules
 
 cd rock-paper-scissors/rps
 npm install
