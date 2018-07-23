324660cb7f905ac2f89f03ee107409458cc4ea00
Safira Nugroho <snugroho@thoughtworks.com>
2018-07-23 23:48:16 +1000

    :))

--- ci/node.sh
+++ ci/node.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-ROOT_FOLDER=$( pwd )
-ln -s $ROOT_FOLDER/node_modules $ROOT_FOLDER/rock-paper-scissors/rps/node_modules
-
 cd rock-paper-scissors/rps
 npm install
 npm run-script build
